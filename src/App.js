77354073761b888e864031bad41aaeaa8c4b31d8
web3santa <usefulstory35@gmail.com>
2024-02-22 03:58:03 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import PageButton from "./components/PageButton";
+import ConnectButton from "./components/ConnectButton";
+
+function App() {
+  const [provider, setProvier] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [signerAddress, setSignerAddress] = useState(undefined);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvier(provider);
+    };
+    onLoad();
+  }, []);
+
+  const getSigner = async (provider) => {
+    provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setSigner(signer);
+  };
+
+  const isConnected = () => signer !== undefined;
+  const getWalletAddress = () => {
+    signer.getAddress().then((address) => {
+      setSignerAddress(address);
+
+      // todo: connect weth and uni contracts
+    });
+  };
+
+  if (signer !== undefined) {
+    getWalletAddress();
+  }
+
+  return (
+    <div className="App">
+      <div className="appNav">
+        <div className="my-2 buttonContainer buttonContainerTop">
+          <PageButton name={"Swap"} isBold={true} />
+          <PageButton name={"Pool"} />
+          <PageButton name={"Vote"} />
+          <PageButton name={"Charts"} />
+        </div>
+
+        <div className="rightNav">
+          <div className="connectButtonContainer">
+            <ConnectButton
+              provider={provider}
+              isConnected={isConnected}
+              signerAddress={signerAddress}
+              getSigner={getSigner}
+            />
+          </div>
+          <div className="my-2 buttonContainer">
+            <PageButton name={"..."} isBold={true} />
+          </div>
+        </div>
+      </div>
+      
+      <div className="appBody">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
